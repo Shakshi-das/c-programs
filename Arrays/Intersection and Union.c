223b8fcf62e7b6d758ddda2718c7284e50998b66
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-02-22 23:13:59 +0530

    Update Intersection and Union.c

--- Arrays/Intersection and Union.c
+++ Arrays/Intersection and Union.c
@@ -7,11 +7,8 @@ int main()
 {
 
     int a[100], b[100];
-
     int m = 0, n = 0;
-
     printf("Enter size of first array: ");
-
     scanf("%d", &m);
 
     for (int i = 0; i < m; i++)
@@ -19,13 +16,11 @@ int main()
     {
 
         printf("Enter a number: ");
-
         scanf("%d", &a[i]);
 
     }
 
     printf("Enter size of second array: ");
-
     scanf("%d", &n);
 
     for (int i = 0; i < n; i++)
@@ -33,17 +28,13 @@ int main()
     {
 
         printf("Enter a number: ");
-
         scanf("%d", &b[i]);
 
     }
 
     int i = 0;
-
     int j = 0;
-
     printf("The union of the two sets: ");
-
     while (i < m && j < n)
 
     {
@@ -69,21 +60,16 @@ int main()
     }
 
     while (i < m)
-
         printf(" %d ", a[i++]);
 
     while (j < n)
-
         printf(" %d ", b[j++]);
 
     printf("\n");
 
     i = 0;
-
     j = 0;
-
     printf("The intersection of the two sets: ");
-
     while (i < m && j < n)
 
     {
