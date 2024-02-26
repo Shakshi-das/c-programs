a8cc989a61af8de07d9cede38f56d4e01b711b15
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-02-26 23:36:41 +0530

    Update Integer and float members.c

--- Structures/Integer and float members.c
+++ Structures/Integer and float members.c
@@ -8,7 +8,6 @@ typedef struct
 {
 
     int x;
-
     float y;
 
 }xyz;
@@ -20,19 +19,12 @@ int main()
     xyz v1,v2,v3;
 
     printf("Enter the values of v1\n");
-
     scanf("%d%f",&v1.x,&v1.y);
-
     printf("Enter the values of v2\n");
-
     scanf("%d%f",&v2.x,&v2.y);
-
     v3.x = v1.x + v2.x;
-
     v3.y = v1.y+v2.y;
-
     printf("The addition is %d and %f ",v3.x,v3.y);
-
     return 0;
 
 }
