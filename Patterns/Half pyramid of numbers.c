40489bd4e5c3a1e8e3c420f9e50431887884c543
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-03-10 23:57:31 +0530

    Update Half pyramid of numbers.c

--- Patterns/Half pyramid of numbers.c
+++ Patterns/Half pyramid of numbers.c
@@ -5,23 +5,15 @@
 int main() {
 
     int rows, i, j;
-
     printf("Enter Number Of Rows You Want: ");
-
     scanf("%d", &rows);
 
- 
-
     for(i=1; i<=rows; i++) {
-
        for(j=1; j<=i; j++) {
-
            printf("%d ", j);
-
        }
 
     printf("\n");
-
   }
 
   return 0;
