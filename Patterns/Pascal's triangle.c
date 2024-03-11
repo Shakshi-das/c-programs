bb55ee8f734f909c293c0a919232ef9d2c40f39e
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-03-11 23:50:25 +0530

    Update Pascal's triangle.c

--- Patterns/Pascal's triangle.c
+++ Patterns/Pascal's triangle.c
@@ -5,35 +5,23 @@
 int main() {
 
    int rows, c = 1, k, i, j;
-
    printf("Enter the number of rows: ");
-
    scanf("%d", &rows);
 
    for (i = 0; i < rows; i++) {
-
       for (k = 1; k <= rows - i; k++)
-
          printf("  ");
-
       for (j = 0; j <= i; j++) {
-
          if (j == 0 || i == 0)
-
             c = 1;
-
          else
-
             c = c * (i - j + 1) / j;
-
          printf("%4d", c);
-
       }
 
       printf("\n");
 
    }
-
     return 0;
 
 }
