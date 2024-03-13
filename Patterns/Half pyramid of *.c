25a4d664c00ac4df3325a0fe107600b08a64ec3d
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-03-13 23:13:13 +0530

    Update Half pyramid of *.c

--- Patterns/Half pyramid of *.c
+++ Patterns/Half pyramid of *.c
@@ -7,7 +7,9 @@ int main() {
    printf("Enter the number of rows: ");
    scanf("%d", &rows);
    for (i = 1; i <= rows; ++i) {
+      
       for (j = 1; j <= i; ++j) {
+         
          printf("* ");
       }
       printf("\n");
