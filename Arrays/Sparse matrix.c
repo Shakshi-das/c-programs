ca9392b0a74a39f8c69ed5c4afdb73fb3d12a658
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-03-22 23:58:50 +0530

    Update Sparse matrix.c

--- Arrays/Sparse matrix.c
+++ Arrays/Sparse matrix.c
@@ -1,27 +1,19 @@
 //program to check if a matrix is sparse matrix or not
 
 #include<stdio.h>
-
 #include<stdlib.h>
 
 int sparse(int m,int n){
 
     int arr[10][10];
-
     int i,j;
-
     int count=0;
 
     for(i=0;i<m;i++){
-
         for(j=0;j<n;j++){
-
             scanf("%d",&arr[i][j]);
-
             if (arr[i][j]==0){
-
                 count=count+1;
-
             }
 
         }
