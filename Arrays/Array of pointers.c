47fbb1eff2790a73a2d4e5dcdb3173121e3391c5
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-03-24 23:44:40 +0530

    Update Array of pointers.c

--- Arrays/Array of pointers.c
+++ Arrays/Array of pointers.c
@@ -17,33 +17,24 @@ int main()
     for (i = 0; i < n; i++)
 
     {
-
         scanf("%d",&a[i]);
-
     }
 
     for (i = 0; i < n; i++)
 
     {
-
         b[i] = &a[i];
-
     }
 
     for (i = 0; i < n; i++)
 
     {
-
         sum += (*b[i]);
-
     }
 
     printf("The sum of the elements are %d",sum);
-
     free(b);
-
     free(a);
-
     return 0;
 
 }
