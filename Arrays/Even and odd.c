faf81810de0d7e0436f5ed98926f8ff6794e5ba8
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-03-26 23:46:59 +0530

    Update Even and odd.c

--- Arrays/Even and odd.c
+++ Arrays/Even and odd.c
@@ -8,17 +8,13 @@ int main()
 {
 
     int arr[100],even[100],odd[100],e = 0,o = 0,n = 0;
-
     printf("Enter the size of array: ");
-
     scanf("%d",&n);
 
     for (int i = 0; i < n; i++)
 
     {
-
         printf("Enter the number: ");
-
         scanf("%d",&arr[i]);
 
     }
@@ -26,23 +22,17 @@ int main()
     for (int i = 0; i < n; i++)
 
     {
-
         if(arr[i]%2 == 0)
 
         {
-
             even[e] = arr[i];
-
             e++;
-
         }
 
         else
-
         {
 
             odd[o] = arr[i];
-
             o++;
 
         }
@@ -50,15 +40,12 @@ int main()
     }
 
     printf("The count of even numbers found: %d.\n",e);
-
     printf("The count of odd numbers found: %d.\n",o);
-
     printf("Elements of Even array: \n");
 
     for (int i = 0; i < e; i++)
 
     {
-
         printf("%d\n",even[i]);
 
     }
