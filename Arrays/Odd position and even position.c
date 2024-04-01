8681d6a9a8d7dd15fb3b70407403fac8e9420a20
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-04-01 23:43:37 +0530

    Update Odd position and even position.c

--- Arrays/Odd position and even position.c
+++ Arrays/Odd position and even position.c
@@ -19,43 +19,32 @@ int prime()
 {
 
     static int a = 2;
-
     int e = 0;
-
     int c = 0;
 
     while(1)
-
     {
 
         c = 0;
-
         for (int i = 1; i <= a/2; i++)
-
         {
 
             if(a%i == 0)
-
                 c++;
 
         }
 
         if(c != 1)
-
             a++;
 
         else 
-
             break;
 
     }
 
     if(c == 1)
-
         e = a;
-
-    a++;
-
+        a++;
     return e;
 
 }
@@ -65,21 +54,16 @@ int main()
 {
 
     int arr[100] ,n = 0;
-
     printf("Enter the size of the array: ");
-
     scanf("%d",&n);
 
     for (int i = 0; i < n; i++)
-
     {
 
         if(i%2 == 0)
-
             arr[i] = prime();
 
         else
-
             arr[i] = fibo();
 
     }
@@ -87,11 +71,8 @@ int main()
     printf("The resultant array: \n");
 
     for (int i = 0; i < n; i++)
-
     {
-
         printf("%d ",arr[i]);
-
     }
 
     return 0;
