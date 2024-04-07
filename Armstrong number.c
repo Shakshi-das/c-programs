463211d4bb2977e88c0ddfc34f238a745f3b0a96
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-04-07 23:46:10 +0530

    Update Armstrong number.c

--- Armstrong number.c
+++ Armstrong number.c
@@ -5,11 +5,8 @@
 int main() { 
 
 int n,num,r,ans=0; 
-
 printf("Enter a positive integer: "); 
-
 scanf("%d", &num); 
-
 n=num; 
 
 do 
@@ -17,9 +14,7 @@ do
 { 
 
 r=n%10; 
-
 ans=ans+r*r*r; 
-
 n=n/10; 
 
 }while(n>0); 
@@ -27,17 +22,13 @@ n=n/10;
 if(ans==num) 
 
 { 
-
 printf("%d is an Armstrong number.",num); 
-
 } 
 
 else 
 
 { 
-
 printf("%d is not an Armstrong number.",num); 
-
 } 
 
 return 0; 
