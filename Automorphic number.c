2287f4b27277698be02747bb0bb71ad8b1eb5b0a
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-04-08 23:53:35 +0530

    Update Automorphic number.c

--- Automorphic number.c
+++ Automorphic number.c
@@ -5,33 +5,23 @@
 int main() {
 
     int num, sqr, temp, last;
-
     int n =0;
-
     printf("Enter a number \n");
-
     scanf("%d",&num);
-
     sqr = num*num;  
-
     temp = num;
-
  
     while(temp>0){
 
         n++;
-
         temp = temp/10;
 
     }
 
  
-
     int den = floor(pow(10,n));
-
     last = sqr % den;
 
- 
     if(last == num)
 
         printf("Automorphic number \n");
