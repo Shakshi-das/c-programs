35743cb228d7262b223b80971e834738b51a72bc
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-04-14 23:58:41 +0530

    Update Sum of digits.c

--- Sum of digits.c
+++ Sum of digits.c
@@ -5,23 +5,18 @@
 int main() {
 
     int N, R, S=0;
-
     printf("\n Please Enter any number\n");
-
     scanf("%d", &N);
 
     while(N > 0)
     {
 
        R= N % 10;
-
        S= S+ R;
-
        N= N / 10;
     }
 
     printf("\n Sum of the digits of Given Number = %d", S);
-
     return 0;
 
 }
