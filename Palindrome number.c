de346ee7c672a3dc17a6bc7b50ce59d570e3f207
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-04-15 23:54:36 +0530

    Update Palindrome number.c

--- Palindrome number.c
+++ Palindrome number.c
@@ -4,22 +4,16 @@
 
 int main() {
 
-  int n, rev = 0, r, s;
-
+    int n, rev = 0, r, s;
     printf("Enter an integer: ");
-
     scanf("%d", &n);
-
     s = n;
 
     // reversed integer is stored in reversed variable
-
     while (n != 0) {
 
         r = n % 10;
-
         rev = rev * 10 + r;
-
         n = n / 10;
 
     }
@@ -27,11 +21,9 @@ int main() {
     // palindrome if orignal and reversed are equal
 
     if (s == rev)
-
         printf("%d is a palindrome.", s);
 
     else
-
         printf("%d is not a palindrome.", s);
 
     return 0;
