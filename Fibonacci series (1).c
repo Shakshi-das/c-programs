c446c98cf1502f9639fb9a3f592c01cb97470e60
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-04-17 23:55:34 +0530

    Update Fibonacci series (1).c

--- Fibonacci series (1).c
+++ Fibonacci series (1).c
@@ -5,28 +5,20 @@
 int main() {
 
     int t1 = 0, t2 = 1, c = 0, n;
-
     printf("Enter a positive number: ");
-
     scanf("%d", &n);
-
     printf("Fibonacci Series: %d, %d, ", t1, t2);
-
     c = t1 + t2;
 
     while (c <= n) {
 
-    printf("%d, ", c);
+       printf("%d, ", c);
 
     t1 = t2;
-
     t2 = c;
-
     c = t1 + t2;
 
   }
-
-  
 
   return 0;
 
