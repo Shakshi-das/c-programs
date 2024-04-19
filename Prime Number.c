366224421157d43eb7cafdde20dd094d708d546f
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-04-19 23:52:40 +0530

    Update Prime Number.c

--- Prime Number.c
+++ Prime Number.c
@@ -5,21 +5,15 @@
 int main() {
 
      int n, i, f = 0;
-
      printf("Enter a positive integer: ");
-
      scanf("%d", &n);
 
      if (n == 0 || n == 1)
-
      f= 1;
 
      for (i = 2; i <= n / 2; ++i) {
-
      if (n % i == 0) {
-
       f= 1;
-
       break;
 
     }
@@ -27,14 +21,11 @@ int main() {
   }
 
      if (f== 0)
-
        printf("%d is a prime number.", n);
 
      else
-
        printf("%d is not a prime number.", n);
 
      return 0;
 
 }
-
