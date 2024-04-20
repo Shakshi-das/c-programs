472cd7ac924cdfdb9a7101146109824f5df16f94
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-04-20 23:55:30 +0530

    Update Number of days.c

--- Number of days.c
+++ Number of days.c
@@ -5,13 +5,10 @@
 int main() {
 
     int month;
-
     printf("enter the number of month:\n");
-
     scanf("%d",&month);
 
     if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-
         printf("Number of days is 31");
 
     else if(month == 2)
