cbb99089f382b30a8935d0778f81024fd8839404
Shakshi Das <96143224+Shakshi-das@users.noreply.github.com>
2024-04-27 23:53:09 +0530

    Update Prime Fibonacci numbers.c

--- Functions/Prime Fibonacci numbers.c
+++ Functions/Prime Fibonacci numbers.c
@@ -8,23 +8,17 @@ int prime(int n)
 {
 
     int count = 0;
-
     for (int i = 1; i <= n/2; i++)
 
     {
-
         if(n%i == 0)
-
             count++;
-
     }
 
     if(count!=1)
-
         return 0;
 
     else 
-
         return 1;
 
 }
@@ -34,23 +28,16 @@ int main()
 {
 
     int n = 0;
-
     printf("Enter the limit: ");
-
     scanf("%d",&n);
-
     int n1 = 0, n2 = 1, n3 = 0;
-
     printf("The prime numbers in fibonacci series: \n");
 
     for (int i = 2; i < n; i++)
-
     {
 
         n3=n1+n2;
-
         n1=n2;
-
         n2=n3;
 
         if(prime(n3) == 1)
